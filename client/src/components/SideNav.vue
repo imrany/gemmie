8703c6715ab2c3fea061f7fe9acf0e02ea7a5b39
imrany <imranmat254@gmail.com>
2024-04-02 17:06:46 +0300

    New ui updates

---
+++ client/src/components/SideNav.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+    let props=defineProps<{
+        data:{
+            res:{
+                text:string,
+                prompt?:string
+            }[],
+            parsedUserDetails:{
+                username:string
+            }
+        },
+        functions:{
+            setShowInput:any,
+        }
+    }>()
+    function reload(){
+        window.location.reload()
+    }
+    function clear(){
+        localStorage.removeItem("chats");
+        reload()
+    }
+    function handleSubmit(e:any){
+        e.preventDefault()
+        let userDetails={
+            username:e.target.username.value
+        }
+        localStorage.setItem("userdetails",JSON.stringify(userDetails))
+        e.target.reset()
+        reload()
+    }
+</script>
+<template>
+    <div class="w-[300px] bg-white z-5 fixed top-0 left-0 bottom-0 border-r-[1px]">
+        <div class="flex items-center justify-between p-3">
+            <p class="font-semibold text-xl text-black">Gemini</p>
+            <div class="flex gap-2 items-center">
+                <button @click="props.functions.setShowInput" title="Open a new chat" class="w-[30px] h-[30px] flex items-center justify-center hover:bg-gray-100 rounded-[50px] cursor-pointer">
+                    <span class="pi pi-plus-circle text-sm"></span>
+                </button>
+                <button @click="reload" title="Refresh Page" class="w-[30px] h-[30px] flex items-center justify-center hover:bg-gray-100 rounded-[50px] cursor-pointer">
+                    <span class="pi pi-refresh text-sm"></span>
+                </button>
+                <button v-if="props.data.res.length!==0" @click="clear" title="Clear Chats" class="w-[30px] h-[30px] flex items-center justify-center hover:bg-gray-100 rounded-[50px] cursor-pointer">
+                    <span class="pi pi-trash text-sm"></span>
+                </button>
+            </div>
+        </div>
+        <div v-if="props.data.res.length!==0&&props.data.parsedUserDetails.username.length!==0" class="flex items-center px-4 cursor-pointer bg-slate-200 h-[70px]">
+            <div class="flex gap-2 items-center flex-grow">
+                <div class="w-[40px] h-[40px] flex justify-center items-center bg-gray-100 rounded-[50px]">
+                    <span class="pi pi-user text-sm"></span>
+                </div>
+                <div class="flex flex-col text-xs gap-1">
+                    <p>{{props.data.parsedUserDetails.username}}</p>
+                    <p>You: {{ props.data.res.slice(props.data.res.length-1,props.data.res.length)[0].prompt }}</p>
+                </div>
+            </div>
+            <div class="ml-auto text-xs">
+                <p class="text-xs capitalize">Today</p>
+            </div>
+        </div>
+        <div v-else-if="props.data.parsedUserDetails.username===undefined||props.data.parsedUserDetails.username.length===0" class="flex flex-col items-center text-sm h-[70vh] w-full px-4 cursor-pointer justify-center">
+            <form @submit="handleSubmit" class="flex gap-2 justify-center flex-col">
+                <input required id="username" name="username" type="text" class="w-[250px] focus:outline-none active:outline-none outline-none border-none focus:border-none px-3 bg-gray-100 placeholder:text-gray-500 text-sm h-[35px] flex-grow py-1 rounded-md" placeholder="Enter your username"/>
+                <button class="rounded-md flex justify-center items-center bg-gray-800 text-white h-[37px] w-fit px-5">Submit</button>
+            </form>
+        </div>
+        <div v-else-if="props.data.parsedUserDetails.username.length!==0" class="flex flex-col h-[70vh] px-3 text-sm justify-center">
+            <div class="flex justify-center flex-col">
+                <p class="text-lg font-semibold">Hello {{ props.data.parsedUserDetails.username }},</p>
+                <p class="text-sm">Welcome to Gemmie, you are using Gemmie Demo, Gemini API Hackathon.</p>
+                <button @click="props.functions.setShowInput()" class="rounded-md flex justify-center items-center bg-gray-200 h-[40px] w-fit mt-2 px-3">Try it now</button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ client/src/components/TopNav.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+let props=defineProps<{
+    data:{
+        res:any,
+        note: string,
+        parsedUserDetails:{
+            username:string
+        }
+    }
+}>()
+</script>
+<template>
+    <div class="h-[44px] bg-white z-5 left-[300px] fixed top-0 right-0 border-b-[1px]">
+        <div class="flex px-5 py-3 items-center justify-between w-full">
+            <p v-if="props.data.parsedUserDetails.username!==undefined" class="text-black text-sm font-semibold">{{props.data.parsedUserDetails.username}}</p>
+            <p v-else class="text-black text-sm font-semibold">Gemmie</p>
+        </div>
+        <div class="bg-blue-200 flex items-center justify-center text-xs h-[20px]">
+            <p>{{ props.data.note }}</p>
+        </div>
+    </div>
+</template>
