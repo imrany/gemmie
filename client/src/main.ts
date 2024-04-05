5cfd064342be9a3cbc9bbc097a33942722edec68
imrany <imranmat254@gmail.com>
2024-04-05 13:37:02 +0300

    removed vue slick

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,9 +3,6 @@ import router from './router'
 import App from './App.vue'
 import 'primeicons/primeicons.css';
 import './assets/main.css'
-// import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-import "vue-slick-ts/dist/css/slick.css";
 
 const app = createApp(App)
 app.use(router)
