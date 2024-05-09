9be2ef6357497ca556f3078c70b6b4628e572b06
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-09 14:29:42 +0300

    refactor

--- gemmie/gemmiev1/src/Components/Sidebar/Sidebar.jsx
+++ gemmie/gemmiev1/src/Components/Sidebar/Sidebar.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar_container">
+      <div className="sidebar_title">
+        <h3>Gemmie</h3>
+        <ion-icon name="refresh-outline"></ion-icon>
+      </div>
+
+      <div className="sidebar_input">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Enter your username"
+        />
+
+        <button type="submit">Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
