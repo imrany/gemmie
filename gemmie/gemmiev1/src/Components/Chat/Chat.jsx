331589959a3b604c0e3121cf669606b7e9254a33
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-09 15:01:43 +0300

    ChatComponent

--- gemmie/gemmiev1/src/Components/Chat/Chat.jsx
+++ gemmie/gemmiev1/src/Components/Chat/Chat.jsx
@@ -4,12 +4,15 @@ import "./Chat.css";
 const Chat = () => {
   return (
     <div className="chat_container">
-      <div className="chat_username">
-        <h3>Gemmie</h3>
+      <div className="chat_title">
+        <div className="chat_username">
+          <h3>Gemmie</h3>
+        </div>
+        <div className="chat_header">
+          <p>You are using Gemmie Demo, Google AI Hackathon</p>
+        </div>
       </div>
-      <div className="chat_header">
-        <p>You are using Gemmie Demo, Google AI Hackathon</p>
-      </div>
+
       <div className="chat_body">
         <ion-icon name="chatbubble-outline"></ion-icon>
         <h1>Gemmie</h1>
