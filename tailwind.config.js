ff4f446f3a6f7e4d50b5d30d7dd4c1540b16a766
imrany <imranmat254@gmail.com>
2025-10-05 21:19:34 +0300

    feat: major editor ui update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'media', // or false or 'class'
+    darkMode: 'class', // or false or 'class' or 'media'
     theme: {
        extend: {},
     },
