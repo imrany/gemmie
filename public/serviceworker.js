0701dd56f0eb71695c461852dba1b663fb7d309f
imrany <imranmat254@gmail.com>
2025-10-20 19:01:01 +0300

    fix: fixed text hightlight popover browser default behaviour

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v0.8.11'; 
+const CACHE_VERSION = 'v0.8.12'; 
 const staticCacheName = `site-static-${CACHE_VERSION}`;
 const dynamicCache = `site-dynamic-${CACHE_VERSION}`;
 
