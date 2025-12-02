b340636d07bdc36199f3959985a8009576ba66e4
imrany <imranmat254@gmail.com>
2025-12-02 22:36:53 +0300

    fix: fixed migration v10

--- gemmie-server/store/migrations/000009_add_chat_id_into_arcades.up.sql
+++ gemmie-server/store/migrations/000009_add_chat_id_into_arcades.up.sql
@@ -1,5 +1,5 @@
  -- added chat_id into arcades
- ALTER TABLE arcades ADD COLUMN chat_id TEXT REFERENCES chats(id) NOT NULL;
+ ALTER TABLE arcades ADD COLUMN chat_id TEXT REFERENCES chats(id) DEFAULT '';
 
   -- added a unique constraint to make sure each chat only has one arcade
-  ALTER TABLE arcades ADD CONSTRAINT unique_chat_id UNIQUE (chat_id),;
+  ALTER TABLE arcades ADD CONSTRAINT unique_chat_id UNIQUE (chat_id);
--- gemmie-server/store/migrations/000010_add_chat_id_into_arcades.up.sql
+++ gemmie-server/store/migrations/000010_add_chat_id_into_arcades.up.sql
@@ -1,5 +1,7 @@
  -- added chat_id into arcades
- ALTER TABLE arcades ADD COLUMN chat_id TEXT REFERENCES chats(id) NOT NULL;
+ ALTER TABLE arcades ADD COLUMN chat_id TEXT REFERENCES chats(id);
+ UPDATE arcades SET chat_id = NULL WHERE chat_id = '';
+ ALTER TABLE arcades ALTER COLUMN chat_id DROP DEFAULT;
 
   -- added a unique constraint to make sure each chat only has one arcade
-  ALTER TABLE arcades ADD CONSTRAINT unique_chat_id UNIQUE (chat_id),;
+  ALTER TABLE arcades ADD CONSTRAINT unique_chat_id UNIQUE (chat_id);
